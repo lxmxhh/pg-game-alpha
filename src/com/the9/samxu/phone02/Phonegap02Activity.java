b7f01184bd8bf49afd1ef7930428e49dddb3707b
lxmxhh <lxmxhh@hotmail.com>
2012-06-20 16:28:17 +0800

    first commit

---
+++ src/com/the9/samxu/phone02/Phonegap02Activity.java
@@ -0,0 +1,14 @@
+package com.the9.samxu.phone02;
+
+import org.apache.cordova.*;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Phonegap02Activity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/mushroom.html");
+    }
+}
